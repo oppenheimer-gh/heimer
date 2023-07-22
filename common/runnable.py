f78ad3378c9ff0e9c502b3983fb72029b299c4c0
Arkan Alexei Andrei <arkan.alexei@ui.ac.id>
2023-07-22 11:40:14 +0700

    remove unneeded files

--- common/runnable.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Runnable(ABC):
-    @classmethod
-    @abstractmethod
-    def run(cls, **kwargs) -> object:
-        pass
--- user/services.py
+++
@@ -1,9 +0,0 @@
-from common.runnable import Runnable
-from user.models import Mentor, Mentee
-
-
-class MatchMentorAndMenteeService(Runnable):
-
-    @classmethod
-    def run(cls, mentor: Mentor, mentee: Mentee) -> None:
-        pass
